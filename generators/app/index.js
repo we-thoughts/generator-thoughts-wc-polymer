c706b285fec8df2f659453ee3db8ac3ea41ff620
kongxiangyan <kcigaret@outlook.com>
2019-08-10 17:51:18 +0800

    change .gitignore to gitignore

--- generators/app/index.js
+++ generators/app/index.js
@@ -158,7 +158,7 @@ module.exports = class extends Generator {
       }
     );
     this.fs.copy(
-      this.templatePath(".gitignore"),
+      this.templatePath("gitignore"),
       this.destinationPath(".gitignore")
     );
     this.fs.copyTpl(
