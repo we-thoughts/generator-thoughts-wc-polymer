38a19a6f739a97576dc99c9eed2766151e52f72b
kongxiangyan <kcigaret@outlook.com>
2019-08-15 23:47:04 +0800

    lint

--- generators/app/templates/webpack.config.js
+++ generators/app/templates/webpack.config.js
@@ -1,11 +1,12 @@
-const path = require('path');
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require("path");
 
 const config = {
-  entry: './build/<%= component_name %>.js',
+  entry: "./build/<%= component_name %>.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '<%= component_name %>.js'
-  },
-}
+    path: path.resolve(__dirname, "dist"),
+    filename: "<%= component_name %>.js"
+  }
+};
 
 module.exports = config;
